247e162d3a3faea8f5842fcbcea16a2396ce4dc6
Daniël Illouz <daniel.illouz@me.com>
2019-12-10 15:42:48 +0100

    Use expect callback to test if an err is thrown (sync) to avoid false positives

--- src/validate-s3-key.spec.js
+++ src/validate-s3-key.spec.js
@@ -16,61 +16,49 @@ describe('validateS3Key', () => {
       'Invalid S3 key, format must be "audio/standups/:standupId/DD-MM-YYYY/:userId/:filename.webm"';
 
     it('throws with no key', () => {
-      try {
+      expect(() => {
         validateS3Key.standupAudioRecording();
-      } catch (err) {
-        expect(err).toHaveProperty('message', errMsg);
-      }
+      }).toThrow(new Error(errMsg));
     });
 
     it('throws with invalid type', () => {
-      try {
+      expect(() => {
         validateS3Key.standupAudioRecording(
           `image/${validEntity}/${validEntityId}/${validDateKey}/${validUserId}/${validFilename}.${validFileExtension}`
         );
-      } catch (err) {
-        expect(err).toHaveProperty('message', errMsg);
-      }
+      }).toThrow(new Error(errMsg));
     });
 
     it('throws with invalid entity', () => {
-      try {
+      expect(() => {
         validateS3Key.standupAudioRecording(
           `${validType}/books/${validEntityId}/${validDateKey}/${validUserId}/${validFilename}.${validFileExtension}`
         );
-      } catch (err) {
-        expect(err).toHaveProperty('message', errMsg);
-      }
+      }).toThrow(new Error(errMsg));
     });
 
     it('throws with invalid entity ID', () => {
-      try {
+      expect(() => {
         validateS3Key.standupAudioRecording(
           `${validType}/${validEntity}//${validDateKey}/${validUserId}/${validFilename}.${validFileExtension}`
         );
-      } catch (err) {
-        expect(err).toHaveProperty('message', errMsg);
-      }
+      }).toThrow(new Error(errMsg));
     });
 
     it('throws with invalid date key', () => {
-      try {
+      expect(() => {
         validateS3Key.standupAudioRecording(
           `${validType}/${validEntity}/${validEntityId}/111-2222-3/${validUserId}/${validFilename}.${validFileExtension}`
         );
-      } catch (err) {
-        expect(err).toHaveProperty('message', errMsg);
-      }
+      }).toThrow(new Error(errMsg));
     });
 
     it('throws with invalid user ID', () => {
-      try {
+      expect(() => {
         validateS3Key.standupAudioRecording(
           `${validType}/${validEntity}/${validEntityId}/${validDateKey}//${validFilename}.${validFileExtension}`
         );
-      } catch (err) {
-        expect(err).toHaveProperty('message', errMsg);
-      }
+      }).toThrow(new Error(errMsg));
     });
 
     it('throws with invalid filename', () => {
@@ -82,23 +70,19 @@ describe('validateS3Key', () => {
     });
 
     it('throws with invalid file extension', () => {
-      try {
+      expect(() => {
         validateS3Key.standupAudioRecording(
           `${validType}/${validEntity}/${validEntityId}/${validDateKey}/${validUserId}/${validFilename}.mp4`
         );
-      } catch (err) {
-        expect(err).toHaveProperty('message', errMsg);
-      }
+      }).toThrow(new Error(errMsg));
     });
 
     it('does not throw for valid key', () => {
-      try {
+      expect(() => {
         validateS3Key.standupAudioRecording(
           `${validType}/${validEntity}/${validEntityId}/${validDateKey}/${validUserId}/${validFilename}.${validFileExtension}`
         );
-      } catch (err) {
-        expect(err).toHaveProperty('message', errMsg);
-      }
+      }).not.toThrow(new Error(errMsg));
     });
   });
 
@@ -115,61 +99,49 @@ describe('validateS3Key', () => {
       'Invalid S3 key, format must be "audio/standups/:standupId/DD-MM-YYYY/:userId/:filename.mp3"';
 
     it('throws with no key', () => {
-      try {
+      expect(() => {
         validateS3Key.standupTranscodedAudioRecording();
-      } catch (err) {
-        expect(err).toHaveProperty('message', errMsg);
-      }
+      }).toThrow(new Error(errMsg));
     });
 
     it('throws with invalid type', () => {
-      try {
+      expect(() => {
         validateS3Key.standupTranscodedAudioRecording(
           `image/${validEntity}/${validEntityId}/${validDateKey}/${validUserId}/${validFilename}.${validFileExtension}`
         );
-      } catch (err) {
-        expect(err).toHaveProperty('message', errMsg);
-      }
+      }).toThrow(new Error(errMsg));
     });
 
     it('throws with invalid entity', () => {
-      try {
+      expect(() => {
         validateS3Key.standupTranscodedAudioRecording(
           `${validType}/books/${validEntityId}/${validDateKey}/${validUserId}/${validFilename}.${validFileExtension}`
         );
-      } catch (err) {
-        expect(err).toHaveProperty('message', errMsg);
-      }
+      }).toThrow(new Error(errMsg));
     });
 
     it('throws with invalid entity ID', () => {
-      try {
+      expect(() => {
         validateS3Key.standupTranscodedAudioRecording(
           `${validType}/${validEntity}//${validDateKey}/${validUserId}/${validFilename}.${validFileExtension}`
         );
-      } catch (err) {
-        expect(err).toHaveProperty('message', errMsg);
-      }
+      }).toThrow(new Error(errMsg));
     });
 
     it('throws with invalid date key', () => {
-      try {
+      expect(() => {
         validateS3Key.standupTranscodedAudioRecording(
           `${validType}/${validEntity}/${validEntityId}/111-2222-3/${validUserId}/${validFilename}.${validFileExtension}`
         );
-      } catch (err) {
-        expect(err).toHaveProperty('message', errMsg);
-      }
+      }).toThrow(new Error(errMsg));
     });
 
     it('throws with invalid user ID', () => {
-      try {
+      expect(() => {
         validateS3Key.standupTranscodedAudioRecording(
           `${validType}/${validEntity}/${validEntityId}/${validDateKey}//${validFilename}.${validFileExtension}`
         );
-      } catch (err) {
-        expect(err).toHaveProperty('message', errMsg);
-      }
+      }).toThrow(new Error(errMsg));
     });
 
     it('throws with invalid filename', () => {
@@ -181,23 +153,19 @@ describe('validateS3Key', () => {
     });
 
     it('throws with invalid file extension', () => {
-      try {
+      expect(() => {
         validateS3Key.standupTranscodedAudioRecording(
           `${validType}/${validEntity}/${validEntityId}/${validDateKey}/${validUserId}/${validFilename}.mp4`
         );
-      } catch (err) {
-        expect(err).toHaveProperty('message', errMsg);
-      }
+      }).toThrow(new Error(errMsg));
     });
 
     it('does not throw for valid key', () => {
-      try {
+      expect(() => {
         validateS3Key.standupTranscodedAudioRecording(
           `${validType}/${validEntity}/${validEntityId}/${validDateKey}/${validUserId}/${validFilename}.${validFileExtension}`
         );
-      } catch (err) {
-        expect(err).toHaveProperty('message', errMsg);
-      }
+      }).not.toThrow(new Error(errMsg));
     });
   });
 });
