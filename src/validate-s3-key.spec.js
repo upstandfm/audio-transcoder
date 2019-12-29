211a523d578636b9da849f35fe06efd41ff4aec8
Daniël Illouz <daniel.illouz@me.com>
2019-12-29 15:25:19 +0100

    Fetch metadata from S3 to store recording name
    Fixes #13
    
    Rename "filename" to "recordingId" to avoid confusion.

--- src/validate-s3-key.spec.js
+++ src/validate-s3-key.spec.js
@@ -9,11 +9,11 @@ describe('validateS3Key', () => {
     const validEntityId = 'J0W4Z1uE';
     const validDateKey = '12-10-2019';
     const validUserId = 'auth0|5d5582168c110c8f790bh89e';
-    const validFilename = 'today';
+    const validRecordingId = 'XQyaVFWe';
     const validFileExtension = 'webm';
 
     const errMsg =
-      'Invalid S3 key, format must be "audio/standups/:standupId/DD-MM-YYYY/:userId/:filename.webm"';
+      'Invalid S3 key, format must be "audio/standups/:standupId/DD-MM-YYYY/:userId/:recordingId.webm"';
 
     it('throws with no key', () => {
       expect(() => {
@@ -24,7 +24,7 @@ describe('validateS3Key', () => {
     it('throws with invalid type', () => {
       expect(() => {
         validateS3Key.standupAudioRecording(
-          `image/${validEntity}/${validEntityId}/${validDateKey}/${validUserId}/${validFilename}.${validFileExtension}`
+          `image/${validEntity}/${validEntityId}/${validDateKey}/${validUserId}/${validRecordingId}.${validFileExtension}`
         );
       }).toThrow(new Error(errMsg));
     });
@@ -32,7 +32,7 @@ describe('validateS3Key', () => {
     it('throws with invalid entity', () => {
       expect(() => {
         validateS3Key.standupAudioRecording(
-          `${validType}/books/${validEntityId}/${validDateKey}/${validUserId}/${validFilename}.${validFileExtension}`
+          `${validType}/books/${validEntityId}/${validDateKey}/${validUserId}/${validRecordingId}.${validFileExtension}`
         );
       }).toThrow(new Error(errMsg));
     });
@@ -40,7 +40,7 @@ describe('validateS3Key', () => {
     it('throws with invalid entity ID', () => {
       expect(() => {
         validateS3Key.standupAudioRecording(
-          `${validType}/${validEntity}//${validDateKey}/${validUserId}/${validFilename}.${validFileExtension}`
+          `${validType}/${validEntity}//${validDateKey}/${validUserId}/${validRecordingId}.${validFileExtension}`
         );
       }).toThrow(new Error(errMsg));
     });
@@ -48,7 +48,7 @@ describe('validateS3Key', () => {
     it('throws with invalid date key', () => {
       expect(() => {
         validateS3Key.standupAudioRecording(
-          `${validType}/${validEntity}/${validEntityId}/111-2222-3/${validUserId}/${validFilename}.${validFileExtension}`
+          `${validType}/${validEntity}/${validEntityId}/111-2222-3/${validUserId}/${validRecordingId}.${validFileExtension}`
         );
       }).toThrow(new Error(errMsg));
     });
@@ -56,12 +56,12 @@ describe('validateS3Key', () => {
     it('throws with invalid user ID', () => {
       expect(() => {
         validateS3Key.standupAudioRecording(
-          `${validType}/${validEntity}/${validEntityId}/${validDateKey}//${validFilename}.${validFileExtension}`
+          `${validType}/${validEntity}/${validEntityId}/${validDateKey}//${validRecordingId}.${validFileExtension}`
         );
       }).toThrow(new Error(errMsg));
     });
 
-    it('throws with invalid filename', () => {
+    it('throws with invalid recording ID', () => {
       expect(() => {
         validateS3Key.standupAudioRecording(
           `${validType}/${validEntity}/${validEntityId}/${validDateKey}/${validUserId}/.${validFileExtension}`
@@ -72,7 +72,7 @@ describe('validateS3Key', () => {
     it('throws with invalid file extension', () => {
       expect(() => {
         validateS3Key.standupAudioRecording(
-          `${validType}/${validEntity}/${validEntityId}/${validDateKey}/${validUserId}/${validFilename}.mp4`
+          `${validType}/${validEntity}/${validEntityId}/${validDateKey}/${validUserId}/${validRecordingId}.mp4`
         );
       }).toThrow(new Error(errMsg));
     });
@@ -80,7 +80,7 @@ describe('validateS3Key', () => {
     it('does not throw for valid key', () => {
       expect(() => {
         validateS3Key.standupAudioRecording(
-          `${validType}/${validEntity}/${validEntityId}/${validDateKey}/${validUserId}/${validFilename}.${validFileExtension}`
+          `${validType}/${validEntity}/${validEntityId}/${validDateKey}/${validUserId}/${validRecordingId}.${validFileExtension}`
         );
       }).not.toThrow(new Error(errMsg));
     });
@@ -92,11 +92,11 @@ describe('validateS3Key', () => {
     const validEntityId = 'J0W4Z1uE';
     const validDateKey = '12-10-2019';
     const validUserId = 'auth0|5d5582168c110c8f790bh89e';
-    const validFilename = 'today';
+    const validRecordingId = 'today';
     const validFileExtension = 'mp3';
 
     const errMsg =
-      'Invalid S3 key, format must be "audio/standups/:standupId/DD-MM-YYYY/:userId/:filename.mp3"';
+      'Invalid S3 key, format must be "audio/standups/:standupId/DD-MM-YYYY/:userId/:recordingId.mp3"';
 
     it('throws with no key', () => {
       expect(() => {
@@ -107,7 +107,7 @@ describe('validateS3Key', () => {
     it('throws with invalid type', () => {
       expect(() => {
         validateS3Key.standupTranscodedAudioRecording(
-          `image/${validEntity}/${validEntityId}/${validDateKey}/${validUserId}/${validFilename}.${validFileExtension}`
+          `image/${validEntity}/${validEntityId}/${validDateKey}/${validUserId}/${validRecordingId}.${validFileExtension}`
         );
       }).toThrow(new Error(errMsg));
     });
@@ -115,7 +115,7 @@ describe('validateS3Key', () => {
     it('throws with invalid entity', () => {
       expect(() => {
         validateS3Key.standupTranscodedAudioRecording(
-          `${validType}/books/${validEntityId}/${validDateKey}/${validUserId}/${validFilename}.${validFileExtension}`
+          `${validType}/books/${validEntityId}/${validDateKey}/${validUserId}/${validRecordingId}.${validFileExtension}`
         );
       }).toThrow(new Error(errMsg));
     });
@@ -123,7 +123,7 @@ describe('validateS3Key', () => {
     it('throws with invalid entity ID', () => {
       expect(() => {
         validateS3Key.standupTranscodedAudioRecording(
-          `${validType}/${validEntity}//${validDateKey}/${validUserId}/${validFilename}.${validFileExtension}`
+          `${validType}/${validEntity}//${validDateKey}/${validUserId}/${validRecordingId}.${validFileExtension}`
         );
       }).toThrow(new Error(errMsg));
     });
@@ -131,7 +131,7 @@ describe('validateS3Key', () => {
     it('throws with invalid date key', () => {
       expect(() => {
         validateS3Key.standupTranscodedAudioRecording(
-          `${validType}/${validEntity}/${validEntityId}/111-2222-3/${validUserId}/${validFilename}.${validFileExtension}`
+          `${validType}/${validEntity}/${validEntityId}/111-2222-3/${validUserId}/${validRecordingId}.${validFileExtension}`
         );
       }).toThrow(new Error(errMsg));
     });
@@ -139,12 +139,12 @@ describe('validateS3Key', () => {
     it('throws with invalid user ID', () => {
       expect(() => {
         validateS3Key.standupTranscodedAudioRecording(
-          `${validType}/${validEntity}/${validEntityId}/${validDateKey}//${validFilename}.${validFileExtension}`
+          `${validType}/${validEntity}/${validEntityId}/${validDateKey}//${validRecordingId}.${validFileExtension}`
         );
       }).toThrow(new Error(errMsg));
     });
 
-    it('throws with invalid filename', () => {
+    it('throws with invalid recording ID', () => {
       expect(() => {
         validateS3Key.standupTranscodedAudioRecording(
           `${validType}/${validEntity}/${validEntityId}/${validDateKey}/${validUserId}/.${validFileExtension}`
@@ -155,7 +155,7 @@ describe('validateS3Key', () => {
     it('throws with invalid file extension', () => {
       expect(() => {
         validateS3Key.standupTranscodedAudioRecording(
-          `${validType}/${validEntity}/${validEntityId}/${validDateKey}/${validUserId}/${validFilename}.mp4`
+          `${validType}/${validEntity}/${validEntityId}/${validDateKey}/${validUserId}/${validRecordingId}.mp4`
         );
       }).toThrow(new Error(errMsg));
     });
@@ -163,7 +163,7 @@ describe('validateS3Key', () => {
     it('does not throw for valid key', () => {
       expect(() => {
         validateS3Key.standupTranscodedAudioRecording(
-          `${validType}/${validEntity}/${validEntityId}/${validDateKey}/${validUserId}/${validFilename}.${validFileExtension}`
+          `${validType}/${validEntity}/${validEntityId}/${validDateKey}/${validUserId}/${validRecordingId}.${validFileExtension}`
         );
       }).not.toThrow(new Error(errMsg));
     });
